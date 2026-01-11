8427c6592f40f209225042cca3ad7e4a8425fba6
leo-arr <leonardo.rojas.rebolledo@gmail.com>
2026-01-11 00:05:11 -0300

    Agrego nuevas arañas

--- script.js
+++ script.js
@@ -5,19 +5,59 @@ let aranaActual = null;
 const aranas = [
   {
     nombre: "Viuda negra",
-    imagenes: ["imagenes/viuda1.JPG", "imagenes/viuda2.jpg"]
+    imagenes: ["imagenes/viuda1.JPG", "imagenes/viuda2.jpg", "imagenes/viuda3.jpg", "imagenes/viuda4.jpg"]
   },
   {
     nombre: "Araña lobo",
-    imagenes: ["imagenes/lobo1.jpg", "imagenes/lobo2.jpg"]
+    imagenes: ["imagenes/lobo1.jpg", "imagenes/lobo2.jpg", "imagenes/lobo3.jpg", "imagenes/lobo4.jpg", "imagenes/lobo5.jpg", "imagenes/lobo6.jpg", "imagenes/lobo7.jpg", "imagenes/lobo8.jpg", "imagenes/lobo9.jpg", "imagenes/lobo10.jpg", "imagenes/lobo11.jpg"]
   },
   {
     nombre: "Araña de rincón",
-    imagenes: ["imagenes/rincon1.jpg", "imagenes/rincon2.jpg"]
+    imagenes: ["imagenes/rincon1.jpg", "imagenes/rincon2.jpg", "imagenes/rincon3.jpg", "imagenes/rincon4.jpg", "imagenes/rincon5.jpg", "imagenes/rincon6.jpg", "imagenes/rincon7.jpg", "imagenes/rincon8.jpg", "imagenes/rincon9.jpg", "imagenes/rincon10.jpg", "imagenes/rincon11.jpg", "imagenes/rincon12.jpg", "imagenes/rincon13.jpg", "imagenes/rincon14.jpg", "imagenes/rincon15.jpg"]
   },
   {
     nombre: "Tarántula",
-    imagenes: ["imagenes/tarantula1.jpg", "imagenes/tarantula2.jpg"]
+    imagenes: ["imagenes/tarantula1.jpg", "imagenes/tarantula2.jpg", "imagenes/tarantula3.jpg"]
+  },
+  {
+    nombre: "Falsa Viuda",
+    imagenes: ["imagenes/falsa-viuda1.jpg", "imagenes/falsa-viuda2.jpg", "imagenes/falsa-viuda3.jpg", "imagenes/falsa-viuda4.jpg", "imagenes/falsa-viuda5.jpg", "imagenes/falsa-viuda6.jpg", "imagenes/falsa-viuda7.jpg", "imagenes/falsa-viuda8.jpg", "imagenes/falsa-viuda9.jpg", "imagenes/falsa-viuda10.jpg", "imagenes/falsa-viuda11.jpg", "imagenes/falsa-viuda12.jpg", "imagenes/falsa-viuda13.jpg"]
+  },
+  {
+    nombre: "Relojera o patas largas",
+    imagenes: ["imagenes/relojera1.jpg", "imagenes/relojera2.jpg"]
+  },
+  {
+    nombre: "Tigre",
+    imagenes: ["imagenes/tigre1.jpg", "imagenes/tigre2.jpg", "imagenes/tigre3.jpg", "imagenes/tigre4.jpg"]
+  },
+  {
+    nombre: "Austrochilidae",
+    imagenes: ["imagenes/austrochilidae1.jpg"]
+  },
+  {
+    nombre: "Fantasma",
+    imagenes: ["imagenes/fantasma1.jpg"]
+  },
+  {
+    nombre: "Opilon",
+    imagenes: ["imagenes/opilon1.jpg"]
+  },
+  {
+    nombre: "Saltarina",
+    imagenes: ["imagenes/saltarina1.jpg"]
+  },
+  {
+    nombre: "Sicario",
+    imagenes: ["imagenes/sicario1.jpg", "imagenes/sicario2.jpg"]
+  },
+  {
+    nombre: "Tierra",
+    imagenes: ["imagenes/tierra1.jpg"]
+  },
+  {
+    nombre: "Tubo",
+    imagenes: ["imagenes/tubo1.jpg"]
   }
 ];
 
